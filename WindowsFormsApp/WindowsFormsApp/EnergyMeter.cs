bd34da55f82d291a255b1e735477fc5c7f2f3e6e
marillkam <mlpmandorinka@gmail.com>
2025-02-24 09:45:58 +0300

    added a new class EnergyManager

---
+++ WindowsFormsApp/WindowsFormsApp/EnergyMeter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+public class EnergyMeter
+{
+    public string DeviceName { get; set; }
+    public decimal PowerConsumption { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public EnergyMeter(string deviceName, decimal powerConsumption)
+    {
+        DeviceName = deviceName;
+        PowerConsumption = powerConsumption;
+        StartTime = DateTime.Now;
+        EndTime = StartTime;
+    }
+    public void StopMeter()
+    {
+        EndTime = DateTime.Now;
+    }
+    public decimal CalculateEnergy()
+    {
+        if (EndTime == StartTime)
+        {
+            return 0;
+        }
+        var duration = EndTime - StartTime;
+        var hours = duration.Hours + (duration.Minutes / 60m);
+        return PowerConsumption * hours;
+    }
+}
